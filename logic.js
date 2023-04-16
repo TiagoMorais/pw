2fa379610a398b256c333dfeb2344375e2b33f0a
Tiago Morais <a19631@alunos.ipca.pt>
2023-04-16 22:51:38 +0100

    added piano

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
 let sound;
-let waveIndex=0;
-let soundIndex=0;
-sounds=[]
+let waveIndex = 0;
+let soundIndex = 0;
+sounds = [];
 window.addEventListener("load", (event) => {
+  
   const fileSelector = document.getElementById("fileSelect");
   fileSelector.addEventListener("change", (event) => {
     if (event.target.files[0]) {
@@ -15,26 +16,34 @@ window.addEventListener("load", (event) => {
 function set_wavesurfer(file) {
   console.log(1);
   //if(wavesurfer!=undefined){
-    //wavesurfer.destroy();
+  //wavesurfer.destroy();
   //}
   //create section for wave
   createWaveSection();
   wavesurfer = WaveSurfer.create({
     container: `.m${waveIndex}`,
     scrollParent: false,
-    backgroundColor:"#db8e02",
+    backgroundColor: "#db8e02",
+    hideScrollbar: true,
     barWidth: 3,
   });
   waveIndex++;
 
+  sounds.push(wavesurfer);
 
-  sounds.push(wavesurfer)
+  
+  wavesurfer.on("seek", function () {
+    updateSeekTime();
+  });
+
+  wavesurfer.on("audioprocess", function () {
+    updateSeekTime();
+  });
 
   wavesurfer.on("ready", function () {
     wavesurfer.play();
-    wavesurfer.setVolume(0.1);
+    wavesurfer.setVolume(0.07);
   });
-  //wavesurfer.load("soad.mp3");
 
   var reader = new FileReader();
 
@@ -52,8 +61,6 @@ function set_wavesurfer(file) {
 
   // Read File as an ArrayBuffer
   reader.readAsArrayBuffer(file);
-
- 
 }
 
 function addMusic() {
@@ -68,17 +75,88 @@ function play() {
 function stop() {
   sounds[soundIndex].pause();
   sounds[soundIndex].seekTo(0);
-
 }
 
 function pause() {
   sounds[soundIndex].pause();
 }
 
+function updateIndex(newIndex) {
+  index = document.getElementById("indexShow");
+  if (sounds.length == 0) {
+    index.textContent = 0;
+    return;
+  }
+  console.log(newIndex, sounds.length);
+  console.log(newIndex >= sounds.length);
+  soundIndex = newIndex >= sounds.length ? sounds.length - 1 : newIndex;
+  soundIndex = newIndex < 0 ? 0 : soundIndex;
+  index.textContent = soundIndex;
+}
 
-function createWaveSection(){
-  let musicWave = document.createElement('div');
-  musicWave.className = "musicWave m"+waveIndex ;
-  parent = document.getElementsByClassName('MusicWaveContainer')[0];
+function createWaveSection() {
+  let musicWave = document.createElement("div");
+  musicWave.className = "musicWave m" + waveIndex;
+  parent = document.getElementsByClassName("MusicWaveContainer")[0];
   parent.appendChild(musicWave);
 }
+
+const timeID = setInterval(tick, 250);
+function tick() {}
+
+function updateSeekTime() {
+  let currentTime = document.getElementById("currentTime");
+  let duration = document.getElementById("duration");
+
+  currentTime.textContent = parseInt(sounds[soundIndex].getCurrentTime());
+  duration.textContent = parseInt(sounds[soundIndex].getDuration());
+  console.log(sounds[soundIndex].getCurrentTime());
+  console.log(sounds[soundIndex].getDuration());
+}
+
+
+
+
+
+
+
+
+//
+//
+// PIANO
+//
+//
+
+window.addEventListener("load", (event) => {
+  
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+keys.forEach(key => {
+  key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+  if (e.repeat) return
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play()
+  key.classList.add('active')
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
+}
+})
